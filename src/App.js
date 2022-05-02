13d0cc3529202dae3daad0e95eab705dcedab225
legionscript <legionscript@legionscript.com>
2022-05-02 15:20:00 -0400

    Tutorial 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Example from './components/Example'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Paragraph text</p>
+      <Example />
+      <Example />
+      <Example />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Example.js
@@ -0,0 +1,10 @@
+function Example() {
+	return (
+		<>
+			<h1>Example Component</h1>
+			<p>Example component paragraph text</p>
+		</>
+	)
+}
+
+export default Example;
