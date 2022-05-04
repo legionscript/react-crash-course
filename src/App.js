ab2601dd94be9ed1772464fe249f50f4ce80ddc0
legionscript <legionscript@legionscript.com>
2022-05-04 13:00:27 -0400

    Tutorial 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Example from './components/Example'
 
 function App() {
+  const testVariable = 'abc'
+  const testArray = [1, 2, 3, 4, 5, 6, 7]
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
@@ -8,6 +11,28 @@ function App() {
       <Example />
       <Example />
       <Example />
+      {testVariable === 'abc' ? (
+        <p>{testVariable}</p>
+        ) : (
+        <p>AAAAAAA</p>
+      )}
+
+      {testVariable === 'abc' && (
+        <>
+        <p>{testVariable}</p>
+        <p></p>
+        </>
+      )}
+
+      <ul>
+        {testArray.map(num => (
+          <>
+          <li>{num}</li>
+          <p></p>
+          </>
+        ))}
+      </ul>
+      {/*<p>{testVariable}</p>*/}
     </div>
   );
 }
