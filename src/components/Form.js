f527bb2c49defc8de8cb33dd430ce8959197eedb
legionscript <legionscript@legionscript.com>
2022-05-09 09:16:40 -0400

    Tutorial 4

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,21 @@ import React, { useState, useEffect } from 'react';
 const Form = ({ text }) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [selectInput, setSelectInput] = useState('')
 
   useEffect(() => {
-  	console.log(username, password)	
-  }, [username, password])
+  	// console.log(username, password, selectInput)	
+  }, [username, password, selectInput])
+
+  const handleSubmit = e => {
+    // logic for form submission
+    e.preventDefault()
+    console.log('form submit!')
+    console.log(username, password, selectInput)
+  }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
     	<p>{text}</p>
     	<div>
     		<label htmlFor='username'>Username</label>
@@ -21,10 +29,20 @@ const Form = ({ text }) => {
     		<input type='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
     	</div>
 
+      {/*<textarea value={username} />*/}
+
+      <select value={selectInput} onChange={e => setSelectInput(e.target.value)}>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+
+      {/*<input type='file' />*/}
+
     	<div>
     		<input type='submit' value='Login' />
     	</div>
-    </div>
+    </form>
   )
 }
 
